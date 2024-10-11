35609e242e9b287efe52a5c80c428b73e1421bbc
Henrik Eidnes <hen@kvalitetsit.dk>
2024-10-11 04:49:26 +0200

    Add configuration option for update URL

--- os2borgerpc/client/updater.py
+++ os2borgerpc/client/updater.py
@@ -3,12 +3,14 @@
 import traceback
 import sys
 import subprocess
+from os2borgerpc.client.config import get_config, has_config
 
 def update_client():
     """Update the client via pip."""
     try:
+        installUrl = get_config("client_update_url") if has_config("client_update_url") else "git+https://github.com/KvalitetsIT/os2borgerpc-client.git"
         subprocess.check_call(
-            [sys.executable, "-m", "pip", "install", "-U", "git+https://github.com/KvalitetsIT/os2borgerpc-client.git@feature/OS-20-aendring-af-admin-site"] # TODO Change URL to relase version
+            [sys.executable, "-m", "pip", "install", "-U", installUrl ]
         )
         sys.exit(0)
     except subprocess.CalledProcessError:
